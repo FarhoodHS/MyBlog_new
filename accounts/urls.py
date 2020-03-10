2736d987038ec66a0d39980a5a648f5ebac07ccc
Farhood <farhood.1995.hosseinzadeh@gmail.com>
2020-03-10 01:24:32 +0000

    Changes on the server code

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signin/', views.login, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
-    path('login/', views.login, name='signin'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
